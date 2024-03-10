550fefc917a31051f1c87d0e2f4c0b9c3bf3cd65
Enjimneering <162901067+Enjimneering@users.noreply.github.com>
2024-03-10 19:34:32 +0000

    V 1.0 Algorithm Visualizer
    Selection Sort Algorithm Works

--- main.c
+++ main.c
@@ -25,30 +25,30 @@ void InitArray(int _array[] , int _arraySize);
 int findMax(int _array[], int _arraySize);
 int findMin(int _array[], int _arraySize);
 int CalculateScaleFactor(int _array[] , int _arraySize , int _maxValue );
-
+void swapIndexes(int _array[], int index1, int index2);
+void SelectionSort(int _array[] , int _arraySize, SDL_Renderer* _renderer , int _scaleFactor , int _maxArrValue);
 
 //Output
 void PrintArray(int _array[], int _arraySize);
 void DrawState();
 
 
-
-
 int main(int argc, char** argv)
 {
     SDL_SETUP(); // SDL Boilerplate Code
 
-    int array[256];
+    //Create unsorted array of random values 
+    int array[1024];
     InitArray(array, ARRAY_SIZE );
-    PrintArray(array , ARRAY_SIZE);
     int maxArrayValue = findMax(array,ARRAY_SIZE);
-   
-   
-   
-   
+
+    //Setup the Visualiser Screen
     int displayScaleFactor = CalculateScaleFactor(array, ARRAY_SIZE, maxArrayValue);
     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
-    DrawState(array, ARRAY_SIZE, renderer, displayScaleFactor, maxArrayValue);
+
+    DrawState(array, ARRAY_SIZE, renderer, displayScaleFactor, maxArrayValue, -1 , -1 , -1 );
+
+    SelectionSort(array, ARRAY_SIZE, renderer,displayScaleFactor , maxArrayValue);
 
     int running = 1;
     while(running) // program loop
@@ -67,7 +67,7 @@ int main(int argc, char** argv)
             }
         }
         
-        SDL_RenderPresent(renderer);
+    
     }
         
     return 0;
@@ -102,20 +102,28 @@ int SDL_SETUP() // Initialize Video - window & renderer
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    printf("\n SDL initialization complete !");
+    printf("\n SDL Initialization Complete!");
 }
 
+void SDL_ClearWindow(SDL_Renderer* _renderer)
+{
+    SDL_SetRenderDrawColor(_renderer, 0,0,0,255);
+    SDL_RenderClear(_renderer);
+
+}
 void InitArray(int _array[] , int _arraySize)
 {
     time_t current_time;
     srand((unsigned)time (&current_time)); // random seed gen
     
+    printf("\n Creating Array..");
     for (size_t i = 0; i < _arraySize; i++)
     {
-        _array[i] = rand() % 50 + 1;
+        _array[i] = rand() %50 + 1;
     }
+    printf(".");
 
-
+    PrintArray(_array , _arraySize);
 }
 
 void PrintArray(int _array[], int _arraySize)
@@ -131,8 +139,6 @@ void PrintArray(int _array[], int _arraySize)
 
 }
 
-
-
 int CalculateScaleFactor(int _array[] , int _arraySize , int _maxValue )
 {
     int displayScaleFactor = (HEIGHT * 0.95) / _maxValue;
@@ -140,12 +146,12 @@ int CalculateScaleFactor(int _array[] , int _arraySize , int _maxValue )
     return displayScaleFactor; 
 }
 
-void DrawState(int _array[] , int _arraySize, SDL_Renderer* _SDLrenderer , int _scaleFactor , int _maxArrValue)
+
+void DrawState(int _array[] , int _arraySize, SDL_Renderer* _renderer, int _scaleFactor, int _maxArrValue , int red , int blue , int green )
 {
-
+    SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 255); // default to white boxes
     int maxRectHeight = _maxArrValue * _scaleFactor;
-
-
+    
     for (size_t i = 0; i < _arraySize; i++)
     {
        SDL_Rect dataRect;
@@ -155,15 +161,68 @@ void DrawState(int _array[] , int _arraySize, SDL_Renderer* _SDLrenderer , int _
        dataRect.x = i * (WIDTH/(ARRAY_SIZE)) ; 
        dataRect.y =  (_maxArrValue + (_maxArrValue -_array[i] )) * (_scaleFactor) - maxRectHeight + WIDTH/20 ;
 
-       printf("\n Drawn index %d - { %d }, at  [%d , %d] , width = %d , height = %d ", i , _array[i], dataRect.x , dataRect.y , dataRect.h, dataRect.w);
-       SDL_RenderFillRect(_SDLrenderer , &dataRect);
+        printf("\n Drawn index %d - { %d }, at  [%d , %d] , width = %d , height = %d ", i , _array[i], dataRect.x , dataRect.y , dataRect.h, dataRect.w);
+       
+       if ( green == 1){
+        SDL_SetRenderDrawColor(_renderer, 0, 255 , 0 , 255);
+       }
+
+       else if (i == blue){
+        SDL_SetRenderDrawColor(_renderer, 0, 0, 255, 255);
+       }
+
+       if ( i == red){
+        SDL_SetRenderDrawColor(_renderer, 255, 0 , 0 , 255);
+       }
+       SDL_RenderFillRect(_renderer , &dataRect);
+       SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 255); // default to white boxes
     }
     
-
-
 }
 
 
+void SelectionSort(int _array[] , int _arraySize, SDL_Renderer* _renderer , int _scaleFactor , int _maxArrValue)
+{
+    printf("\n Sorting.");
+
+    for(int i = 0; i < _arraySize ; i++)
+    {
+     printf(".");
+
+        for(int j = i + 1 ; j < _arraySize ; j++)
+        {
+            //Clear Canvas
+            SDL_SetRenderDrawColor(_renderer, 0,0,0,255);
+            SDL_RenderClear(_renderer);
+
+            //Set next block to white
+            SDL_SetRenderDrawColor(_renderer, 255,255,255,255);
+             
+                if (_array[j] < _array[i]) // buble sort ???
+                {
+                    swapIndexes( _array  , j, i);
+                    //printf("\n index %d swapped with index %d", j , i);
+                }
+                
+                SDL_Delay(100);
+
+                DrawState(_array, _arraySize, _renderer, _scaleFactor, _maxArrValue, j , i , -1 );
+                SDL_RenderPresent(renderer);
+        }
+            
+    }
+    printf(" Sort Completed! \n ");
+    
+    //Clear Canvas
+    SDL_SetRenderDrawColor(_renderer, 0,0,0,255);
+    SDL_RenderClear(_renderer);
+  
+    //Final Output
+    
+    DrawState(_array, _arraySize, _renderer, _scaleFactor, _maxArrValue, -1 ,-1 , -1 );
+    SDL_RenderPresent(renderer);
+    PrintArray(_array ,_arraySize );
+}
 
 
 
@@ -202,7 +261,12 @@ int findMin(int _array[], int _arraySize)
 
 }
 
-
+void swapIndexes(int _array[], int index1, int index2)
+{
+    int temp = _array[index1];
+    _array[index1] = _array[index2];
+     _array[index2] = temp;
+}
 
 
 
